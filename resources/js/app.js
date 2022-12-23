285830cb066c4900a9eb7aa405add3a0ac56fc90
neemspees (Kim Bouchouaram) <kim@libaro.be>
2022-12-23 12:32:31 +0100

    add snow during December and January

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,12 @@ require('./bootstrap');
 
 import Alpine from 'alpinejs'
 import intersect from '@alpinejs/intersect'
+import snow from './scripts/snow';
 
 Alpine.plugin(intersect)
 
 window.Alpine = Alpine
 
 Alpine.start()
+
+snow();
