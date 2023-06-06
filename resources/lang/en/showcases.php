fe1b6a2dbbde971e11d8346208ced8cfaf5cb38d
Jonas De Smet <jonas@libaro.be>
2023-06-06 15:09:07 +0200

    Add English translations to realisations page intro - T17859

---
+++ resources/lang/en/showcases.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => 'Our realisations',
+    'subtitle' => 'With proud',
+    'intro' => [
+        'first_paragraph' => 'Look at our best solutions we\'ve achieved.',
+        'second_paragraph' => 'You can find our open source packages at <a href="https://github.com/libaro-io" target="_blank">GitHub.',
+    ],
+    'with_tags' => 'Realisations with tag:',
+    'clear_tags' => 'show all realisations',
+];
--- resources/lang/nl/showcases.php
+++ resources/lang/nl/showcases.php
@@ -1,1 +1,12 @@
 <?php
+
+return [
+    'title' => 'Onze realisaties',
+    'subtitle' => 'Met trots',
+    'intro' => [
+        'first_paragraph' => 'Bekijk hier de beste oplossingen die wij hebben gerealiseerd.',
+        'second_paragraph' => 'Onze open source packages vind je op <a href="https://github.com/libaro-io" target="_blank">GitHub.',
+    ],
+    'with_tags' => 'Realisaties met tag:',
+    'clear_tags' => 'toon alle realisaties',
+];
