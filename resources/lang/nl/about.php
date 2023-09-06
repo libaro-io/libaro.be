99beb05d01545caf1ba20b1e57d18ce679d7c9b4
Martijn Tetaert <martijn@libaro.be>
2023-09-06 11:19:22 +0200

    Update texts on website

--- resources/lang/nl/about.php
+++ resources/lang/nl/about.php
@@ -2,22 +2,26 @@
 
 return [
     'title' => 'Over Libaro',
-    'intro' => 'Negen jaar is een lange tijd in software-jaren. Genoeg om een team te laten groeien. De mensen die bij Libaro werken zijn leergierige, getalenteerde probleemoplossers die zich toeleggen op het streven naar digitale uitmuntendheid.',
+    'intro' => 'In software-jaren is een decennium een eeuwigheid. Gedurende die tijd hebben we bij Libaro een leuk en getalenteerd team gevormd. Onze mensen zijn leergierige probleemoplossers die uitblinken in digitale innovatie.',
     'kick-off' => [
-        'title' => 'Op zoek naar de grootste digitale uitdagingen',
+        'title' => 'Uw ambities, onze realiteit',
         'subtitle' => 'Kick-Off',
-        'body' => 'Met een onderscheidende expertise en trackrecord zijn we toegewijd om de sterkste, state-of-the-art webapplicaties te ontwikkelen. We brengen de beste digitale ideeën tot werkelijkheid.',
+        'body' => 'Onze expertise en trackrecord spreken voor zich. We zijn niet zomaar een softwarebureau; we zijn uw meest betrouwbare partner voor het omzetten van uw grootste digitale ambities in state-of-the-art webapplicaties. <br> <br>
+Onze technische bekwaamheid vormt de kern van wat we doen, maar we begrijpen dat complexe projecten vaak multidisciplinaire expertise vereisen. Daarom versterken we het team met externe grafische ontwerpers, UX-designers en hardwareontwikkelaars wanneer het project daarom vraagt.',
     ],
     'team' => [
         'title' => 'Focus op resultaat',
         'subtitle' => 'Team',
-        'body' => 'Een team van digital natives houdt van een technische uitdaging. Maar het economische aspect is altijd een belangrijk onderdeel van wat we doen. We leveren tastbare zakelijke voordelen met behulp van vele jaren gecombineerde sectoroverschrijdende ervaring om onze klanten beter, veerkrachtiger en winstgevender te maken. Het team wordt hierin ook opgeleid door te werken op projecten die we zelf in de markt zetten. We zijn ondernemers, net als u.',
+        'body' => 'Een team van digital natives houdt van een technische uitdaging. Maar het economische aspect is altijd een belangrijk onderdeel van wat we doen. Door onze Agile werkwijze heeft u zeer snel tastbaar resultaat van onze ontwikkelingen en bovendien biedt het de flexibiliteit om in te spelen op veranderende behoeften tijdens het project. We investeren ook in onze eigen in-house projecten om ons team voortdurend bij te scholen—omdat we ondernemers zijn, net als u.',
     ],
     'open-source' => [
         'title' => 'Open source community',
         'subtitle' => 'Open Source',
-        'body_1' => 'We publiceerden reeds meerdere open-source packages die we publiek plaatsen voor alle programmeurs die hier gebruik van willen maken.',
-        'body_2' => 'Deze vormen een toolset voor onze eigen projecten.',
-        'body_3' => 'Daarnaast is het schrijven van open-source een manier om te werken aan de kwaliteit van onze code. De volledige open-source community kan namelijk meekijken, wat ons doet streven naar kwalitatieve code volgens de regels van de kunst.',
+        'body' => 'We zijn actieve deelnemers aan de open-source gemeenschap, met meerdere publicaties van tools en packages die iedere ontwikkelaar kan gebruiken. Deze open-source initiatieven dienen niet alleen als toolset voor onze eigen projecten, maar houden ons ook scherp. De open-source gemeenschap kijkt mee, en dat stimuleert ons om code van de hoogste kwaliteit te schrijven.',
+    ],
+    'ai' => [
+        'title' => 'In samenwerking met AI',
+        'subtitle' => 'Half man, half machine ',
+        'body' => 'Bij Libaro omarmen we de kracht van artificiële intelligentie als een waardevol hulpmiddel in ons ontwikkelingsarsenaal. Onze getalenteerde ontwikkelaars worden aangemoedigd om AI volledig te benutten, waardoor ze complexe uitdagingen met meer gemak kunnen aanpakken en sneller hoogwaardige code kunnen schrijven. <br><br>We koesteren de mogelijkheden die AI biedt, maar behouden altijd onze kritische blik om ervoor te zorgen dat elke toepassing onze hoge normen voor kwaliteit, beveiliging en prestaties bereikt. ',
     ],
 ];
