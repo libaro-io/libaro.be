19d99242339518f4d35b2571e3cc143c460769c3
Martijn Tetaert <martijn@libaro.be>
2023-12-13 16:45:14 +0100

    Fix sitemap test

--- app/Console/Commands/CreateSitemap.php
+++ app/Console/Commands/CreateSitemap.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\LandingPage;
+use App\Models\Showcase;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 use Spatie\Sitemap\SitemapGenerator;
