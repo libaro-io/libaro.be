5ed09050be2243d068249510f71f659e21eba847
Martijn Tetaert <martijn@libaro.be>
2025-01-07 16:28:30 +0100

    Update integration page

--- resources/views/pages/timecard/integration.blade.php
+++ resources/views/pages/timecard/integration.blade.php
@@ -43,7 +43,7 @@
                 de bouwsector. De naadloze integratie met Robaws zorgt ervoor dat alle prestaties zonder moeite verwerkt
                 worden in de werkbonnen, voortgangstaten en facturatie van Robaws. </p>
             <h2 class="font-gilroy font-bold text-3xl text-primary-darkest mt-12 mb-5">{{ __('Wat houdt het precies in?') }}</h2>
-            <p class="font-grotesk font-semibold text-base text-primary-darkest xl:w-2/3">
+            <p class="font-grotesk font-semibold text-xl text-primary-darkest xl:w-2/3">
                 Met deze integratie kunnen teamleiders en werfleiders eenvoudig <strong>urenregistraties</strong>,
                 <strong>materiaalgebruik</strong>,
                 <strong>materieelbeheer</strong>, <strong>afvalregistraties</strong> en <strong>ritten
@@ -51,7 +51,7 @@
                 informatie wordt automatisch verwerkt en dagelijks gesynchroniseerd met het Robaws ERP-systeem, waardoor
                 uw gegevens altijd up-to-date zijn.
             </p>
-            <p class="font-grotesk font-semibold text-base text-primary-darkest mt-5 xl:w-2/3">
+            <p class="font-grotesk font-semibold text-xl text-primary-darkest mt-5 xl:w-2/3">
                 Een belangrijk voordeel van deze integratie is de mogelijkheid om, <strong>in de vorm van een werkbon,
                     alle
                     kosten direct aan specifieke projecten te koppelen</strong>, wat resulteert in een duidelijk
@@ -60,13 +60,13 @@
                 Robaws-account, kun je werkbonnen, voortgangsstaten en facturatie efficiënt beheren, wat leidt tot een
                 nauwkeurige winstberekening.
             </p>
-            <p class="font-grotesk font-semibold text-base text-primary-darkest mt-5 xl:w-2/3">
+            <p class="font-grotesk font-semibold text-xl text-primary-darkest mt-5 xl:w-2/3">
                 Bovendien stelt de <strong>automatische synchronisatie</strong> je in staat om een volledig overzicht te
                 behouden van
                 alle lopende projecten zonder handmatige invoer in meerdere systemen. Indien nodig kunnen gegevens
                 eenvoudig worden aangepast en opnieuw worden verzonden.
             </p>
-            <p class="font-grotesk font-semibold text-base text-primary-darkest mt-5 xl:w-2/3">
+            <p class="font-grotesk font-semibold text-xl text-primary-darkest mt-5 xl:w-2/3">
                 Naast het registreren van werkmiddelen, automatisch koppelen van werkbonnen, etc. biedt de app nog
                 enkele unieke functionaliteiten. Dankzij een slim algoritme doet de app <strong>automatisch een voorstel
                     voor
@@ -79,11 +79,11 @@
             </p>
             <h2 class="font-gilroy font-bold text-3xl text-primary-darkest mt-12 mb-5">{{ __('Voordelen van de integratie') }}</h2>
             <p
-                class="font-grotesk font-semibold text-base text-primary-darkest xl:w-2/3">
+                class="font-grotesk font-semibold text-xl text-primary-darkest xl:w-2/3">
                 De koppeling met Robaws biedt tal van voordelen voor bedrijven die streven naar efficiëntere
                 werkprocessen en een betere projectcontrole:
             </p>
-            <ul class="font-grotesk font-semibold text-base text-primary-darkest mt-5 xl:w-2/3 ml-10 flex flex-col gap-3 list-disc">
+            <ul class="font-grotesk font-semibold text-xl text-primary-darkest mt-5 xl:w-2/3 ml-10 flex flex-col gap-3 list-disc">
                 <li><strong>Efficiënt kostenbeheer:</strong> Houd budgetten scherp in de gaten en voorkom onverwachte overschrijdingen met real-time inzicht in projectkosten.</li>
                 <li><strong>Tijdsbesparing:</strong> Bespaar kostbare uren door gegevens automatisch te registreren en te synchroniseren, waardoor minder handmatige invoer nodig is.</li>
                 <li><strong>Gebruiksvriendelijke interface:</strong> Met een intuïtieve en eenvoudige gebruikerservaring kunnen alle teamleden gemakkelijk hun uren en materiaal registreren.</li>
@@ -91,23 +91,32 @@
                 <li><strong>Naadloze integratie:</strong> De integratie sluit moeiteloos aan op uw bestaande workflows en optimaliseert de samenwerking tussen verschillende afdelingen.</li>
             </ul>
             <h2 class="font-gilroy font-bold text-3xl text-primary-darkest mt-12 mb-5">{{ __('Hoe wordt dit in jouw workflow geïmplementeerd?') }}</h2>
-            <p class="font-grotesk font-semibold text-base text-primary-darkest xl:w-2/3">
+            <p class="font-grotesk font-semibold text-xl text-primary-darkest xl:w-2/3">
                 De implementatie van de integratie is eenvoudig en snel. Het proces omvat de volgende stappen:
             </p>
-            <ol class="font-grotesk font-semibold text-base text-primary-darkest mt-5 xl:w-2/3 ml-10 flex flex-col gap-3 list-decimal">
+            <ol class="font-grotesk font-semibold text-xl text-primary-darkest mt-5 xl:w-2/3 ml-10 flex flex-col gap-3 list-decimal">
                 <li><strong>Activatie:</strong> We zorgen ervoor dat de app wordt aangepast aan jouw bedrijf en gekoppeld wordt aan je bestaande Robaws-account en gegevens.</li>
                 <li><strong>Documentatie:</strong> We bezorgen je duidelijke instructies over het gebruik van de app. Indien nodig zetten we even een korte video call op voor een demo.</li>
                 <li><strong>Ondersteuning en updates:</strong> De integratie wordt continu onderhouden en verbeterd om de beste ervaring te bieden aan alle gebruikers.</li>
             </ol>
-            <p class="font-grotesk font-semibold text-base text-primary-darkest mt-5 xl:w-2/3">
+            <p class="font-grotesk font-semibold text-xl text-primary-darkest mt-5 xl:w-2/3">
                 De implementatie vereist minimale inspanning van uw team en zorgt ervoor dat uw bedrijf vrijwel direct profiteert van een efficiëntere werkwijze.
             </p>
+            <div class="questions">
+                <h2 class="font-gilroy font-bold text-3xl text-primary-darkest mt-12 mb-5">{{ __('Heeft u verdere vragen of ondersteuning nodig?') }}</h2>
+                <a href="{{ route('timecard-support') }}"
+                   class=" text-white font-grotesk font-semibold text-xl py-3 px-6 rounded-full inline-block bg-primary-dark hover:bg-primary-medium">
+                    {{ __('Klik hier') }}
+                </a>
+            </div>
             <div class="images grid grid-cols-1 md:grid-cols-3 gap-4 mt-12 md:max-w-6xl mx-auto">
                 <img src="{{ asset('images/timecard/screenshots/1.png') }}" alt="Timecard App" class="max-w-full mt-12">
                 <img src="{{ asset('images/timecard/screenshots/2.png') }}" alt="Timecard App" class="max-w-full mt-12">
                 <img src="{{ asset('images/timecard/screenshots/3.png') }}" alt="Timecard App" class="max-w-full mt-12">
                 <img src="{{ asset('images/timecard/screenshots/4.png') }}" alt="Timecard App" class="max-w-full mt-12 md:col-span-3">
             </div>
+
+
         </section>
     </main>
 
