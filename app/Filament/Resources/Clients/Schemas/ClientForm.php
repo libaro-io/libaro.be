efac560467ea669db1248b07b13bcacd4569a836
Kevin Rousseau <kevinrousseau@Kevins-MacBook-Pro.local>
2025-09-18 15:13:11 +0200

    Add clients to filament

---
+++ app/Filament/Resources/Clients/Schemas/ClientForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\Clients\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class ClientForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('weight')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                Toggle::make('visible')
+                    ->required(),
+            ]);
+    }
+}
--- app/Filament/Resources/Clients/Tables/ClientsTable.php
+++ app/Filament/Resources/Clients/Tables/ClientsTable.php
@@ -1,26 +1,31 @@
 <?php
 
-namespace App\Filament\Resources\Projects\Tables;
+namespace App\Filament\Resources\Clients\Tables;
 
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Tables\Columns\CheckboxColumn;
-use Filament\Tables\Table;
-use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
 
-class ProjectsTable
+class ClientsTable
 {
     public static function configure(Table $table): Table
     {
         return $table
             ->columns([
-                TextColumn::make('name'),
-                IconColumn::make('visible')->boolean(),
-                IconColumn::make('is_product')->boolean(),
+                TextColumn::make('name')
+                    ->searchable(),
+                IconColumn::make('visible')
+                    ->boolean(),
+                TextColumn::make('weight')
+                    ->numeric()
+                    ->sortable(),
             ])
-            ->filters([])
+            ->filters([
+                //
+            ])
             ->recordActions([
                 EditAction::make(),
             ])
