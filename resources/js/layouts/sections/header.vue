37a45f4cb28c19e5d4a888bf7dd94375bcb73577
Martijn Tetaert <martijn@libaro.be>
2025-09-19 16:06:30 +0200

    Update header

--- resources/js/layouts/sections/header.vue
+++ resources/js/layouts/sections/header.vue
@@ -2,28 +2,59 @@
 
 import PrimaryMenu from "@layouts/sections/primary-menu.vue";
 import SecondaryMenu from "@layouts/sections/secondary-menu.vue";
+import {Ref, ref} from "vue";
 
 const props = withDefaults(defineProps<{
     background?: string;
 }>(), {
-    background: "@assets/images/header_striped.webp"
+    background: "/images/header_striped.webp"
 });
+
+const menuOpen: Ref<boolean> = ref(false);
+
+const toggleMenu = () => {
+    menuOpen.value = !menuOpen.value;
+}
 
 </script>
 <template>
     <section
-
+        :style="{
+            'background-image': 'url(' + props.background + ')'
+        }"
         class="section-header">
        <div class="container">
            <header>
                <div class="logo">
                    <img src="@assets/logos/libaro_logo_full_white_without_tagline.svg" alt="logo">
                </div>
-               <div class="menus">
+               <button class="menu-button" @click="toggleMenu()">
+                   <i
+                       :class="[
+                           'fa-solid',
+                           menuOpen ? 'fa-xmark' : 'fa-bars'
+                        ]"
+                   ></i>
+               </button>
+               <div
+                   :class="[
+                       'menus',
+                       menuOpen ? 'open' : ''
+                   ]"
+               >
+                   <button class="close-button" @click="toggleMenu()">
+                       <i class="fa-solid fa-xmark"></i>
+                   </button>
                    <div class="top">
                        <secondary-menu type="header"></secondary-menu>
                    </div>
                    <primary-menu type="header"></primary-menu>
+               </div>
+               <div
+                   class="overlay"
+                   @click="toggleMenu()"
+                   v-if="menuOpen"
+               >
                </div>
            </header>
        </div>
