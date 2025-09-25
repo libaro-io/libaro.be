80247db55575aeda8ad1835dbc4a926caf3e470e
Kevin Rousseau <kevinrousseau@Kevins-MacBook-Pro.local>
2025-09-25 11:45:37 +0200

    Fix fallback for client image

--- resources/js/components/client-block-component.vue
+++ resources/js/components/client-block-component.vue
@@ -3,16 +3,20 @@ import {Link} from "@inertiajs/vue3";
 import {ClientInterface} from "@interfaces/ClientInterface";
 import ClientRandomProjectController from "@actions/App/Http/Controllers/ClientRandomProjectController";
 import {useS3Image} from "@composables/useS3Image";
+import {computed} from "vue";
 
 const props = defineProps<{
     client: ClientInterface;
 }>();
 
+const image = computed(() => {
+    return props.client.image ? useS3Image(props.client.image) : null;
+});
 </script>
 <template>
     <section class="component-client-block-component">
         <Link prefetch :href="ClientRandomProjectController({client:props.client})">
-            <img :src="useS3Image(props.client.image)" :alt="props.client.name" />
+            <img v-if="image" :src="image" :alt="props.client.name"/>
         </Link>
     </section>
 </template>
