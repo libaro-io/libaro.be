62267705617105a3218c65c770af61c898458860
Martijn Tetaert <martijn@libaro.be>
2025-10-03 14:58:37 +0200

    Add vite prefetch test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
-
+        Vite::prefetch(concurrency: 3);
         Model::preventLazyLoading($this->app->isLocal());
         Model::preventAccessingMissingAttributes($this->app->isLocal());
         Model::preventSilentlyDiscardingAttributes($this->app->isLocal());
