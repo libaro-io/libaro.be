c59261f94ee683bf19a5580900c50b9c23ab005e
Kevin Rousseau <kevinrousseau@Kevins-MacBook-Pro.local>
2025-10-03 09:32:00 +0200

    Replace window occurances

--- resources/js/layouts/website.vue
+++ resources/js/layouts/website.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import NavigationFooter from "@layouts/sections/navigation-footer.vue";
 import Footer from "@layouts/sections/footer.vue";
-import {setUrlDefaults} from "../wayfinder";
-import {Head, usePage} from "@inertiajs/vue3";
-import {computed} from "vue";
-import Header, {HeaderOptions} from "@layouts/sections/header.vue";
+import { setUrlDefaults } from "../wayfinder";
+import { Head, usePage } from "@inertiajs/vue3";
+import { computed } from "vue";
+import Header, { HeaderOptions } from "@layouts/sections/header.vue";
 import PageInterface from "@interfaces/PageInterface";
-import {getTrans} from "@composables/UseTranslationHelper";
-import {SeoMetaKey} from "@composables/UseMetaHelper";
+import { getTrans } from "@composables/UseTranslationHelper";
+import { SeoMetaKey } from "@composables/UseMetaHelper";
 
 const page = usePage<PageInterface>();
 
@@ -41,39 +41,55 @@ const metaDescriptionByMetaKey = computed(() => {
 });
 
 const getTitle = computed(() => {
-    return "Libaro | " + ( props.metaTitleOverride ?? metaTitleByMetaKey.value ?? getTrans("seo.general.tagline"));
+    return (
+        "Libaro | " +
+        (props.metaTitleOverride ??
+            metaTitleByMetaKey.value ??
+            getTrans("seo.general.tagline"))
+    );
 });
 
 const getDescription = computed(() => {
-    const description = props.metaDescriptionOverride ?? metaDescriptionByMetaKey.value ?? getTrans("seo.general.description");
+    const description =
+        props.metaDescriptionOverride ??
+        metaDescriptionByMetaKey.value ??
+        getTrans("seo.general.description");
 
     if (description.length > 170) {
-        return description.substring(0, 167) + '...';
+        return description.substring(0, 167) + "...";
     }
 
     return description;
 });
 
 const getCanonical = computed(() => {
-    //return window.location.origin + window.location.pathname;
+    return page.props.pageProps.url.origin + page.props.pageProps.url.pathname;
 });
 </script>
 <template>
     <Head :title="getTitle">
-        <meta name="description" :content="getDescription"/>
-        <meta name="canonical" :content="getCanonical"/>
+        <meta name="description" :content="getDescription" />
+        <meta name="canonical" :content="getCanonical" />
 
-        <meta property="og:title" :content="getTitle"/>
-        <meta property="og:description" :content="getDescription"/>
+        <meta property="og:title" :content="getTitle" />
+        <meta property="og:description" :content="getDescription" />
 
-        <meta property="twitter:title" :content="getTitle"/>
-        <meta property="twitter:description" :content="getDescription"/>
+        <meta property="twitter:title" :content="getTitle" />
+        <meta property="twitter:description" :content="getDescription" />
     </Head>
     <div id="layout-website">
+        <Header
+            :margin-bottom="props.marginBottom"
+            :page-title="props.pageTitle"
+            :page-sub-title="props.pageSubTitle"
+            :page-description="props.pageDescription"
+            :options="props.headerOptions ?? {}"
+        ></Header>
         <main class="content">
-
             <slot></slot>
         </main>
+        <navigation-footer></navigation-footer>
+        <Footer></Footer>
     </div>
 </template>
 <style scoped>
