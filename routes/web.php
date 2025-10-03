33254e35439a8941fb33c9c53f2546d6d64e1d08
Kevin Rousseau <kevinrousseau@Kevins-MacBook-Pro.local>
2025-10-03 15:45:47 +0200

    Remove responsecache

--- routes/web.php
+++ routes/web.php
@@ -26,45 +26,44 @@ use App\Http\Controllers\SupportTimecardController;
 use App\Http\Controllers\TermsController;
 use App\Http\Controllers\UpdateLangController;
 use Illuminate\Support\Facades\Route;
-use Spatie\ResponseCache\Middlewares\CacheResponse;
 
 // Don't forget to add new routes to the sitemap.blade.php!
 
 Route::prefix('{locale?}')->group(function () {
     Route::post('lang', UpdateLangController::class);
 
-    Route::get('/', HomeController::class)->middleware(CacheResponse::class);
-    Route::get('l/{landingPage:slug}', LandingPageController::class)->middleware(CacheResponse::class);
+    Route::get('/', HomeController::class);
+    Route::get('l/{landingPage:slug}', LandingPageController::class);
 
-    Route::get('contact', ContactController::class)->middleware(CacheResponse::class);
+    Route::get('contact', ContactController::class);
     Route::post('contact', SubmitContactFormController::class);
 
-    Route::get('over-ons', AboutUsController::class)->middleware(CacheResponse::class);
+    Route::get('over-ons', AboutUsController::class);
 
-    Route::get('realisaties', ProjectController::class)->middleware(CacheResponse::class);
-    Route::get('realisaties/{project:slug}', DetailProjectController::class)->middleware(CacheResponse::class);
+    Route::get('realisaties', ProjectController::class);
+    Route::get('realisaties/{project:slug}', DetailProjectController::class);
 
-    Route::get('blog', BlogController::class)->middleware(CacheResponse::class);
-    Route::get('blog/{blog:slug}', DetailBlogController::class)->middleware(CacheResponse::class);
+    Route::get('blog', BlogController::class);
+    Route::get('blog/{blog:slug}', DetailBlogController::class);
 
-    Route::get('producten', ProductController::class)->middleware(CacheResponse::class);
-    Route::get('producten/{product:slug}', DetailProductController::class)->middleware(CacheResponse::class);
+    Route::get('producten', ProductController::class);
+    Route::get('producten/{product:slug}', DetailProductController::class);
 
-    Route::get('privacy', PrivacyPolicyController::class)->middleware(CacheResponse::class);
-    Route::get('cookies', CookiePolicyController::class)->middleware(CacheResponse::class);
-    Route::get('terms', TermsController::class)->middleware(CacheResponse::class);
+    Route::get('privacy', PrivacyPolicyController::class);
+    Route::get('cookies', CookiePolicyController::class);
+    Route::get('terms', TermsController::class);
 
-    Route::get('expertise/web-development', WebDevelopmentExpertiseController::class)->middleware(CacheResponse::class);
-    Route::get('expertise/apps', AppsExpertiseController::class)->middleware(CacheResponse::class);
-    Route::get('expertise/ai-integrations', AiIntegrationsExpertiseController::class)->middleware(CacheResponse::class);
-    Route::get('expertise/iot', IOTExpertiseController::class)->middleware(CacheResponse::class);
-    Route::get('expertise/odoo', OdooExpertiseController::class)->middleware(CacheResponse::class);
-    Route::get('expertise/robaws', RobawsExpertiseController::class)->middleware(CacheResponse::class);
+    Route::get('expertise/web-development', WebDevelopmentExpertiseController::class);
+    Route::get('expertise/apps', AppsExpertiseController::class);
+    Route::get('expertise/ai-integrations', AiIntegrationsExpertiseController::class);
+    Route::get('expertise/iot', IOTExpertiseController::class);
+    Route::get('expertise/odoo', OdooExpertiseController::class);
+    Route::get('expertise/robaws', RobawsExpertiseController::class);
 
-    Route::get('clients/{client}', ClientRandomProjectController::class)->middleware(CacheResponse::class);
+    Route::get('clients/{client}', ClientRandomProjectController::class);
 
-    Route::get('assets', AssetsController::class)->middleware(CacheResponse::class);
+    Route::get('assets', AssetsController::class);
 
-    Route::get('timecard/integration', IntegrationTimecardController::class)->middleware(CacheResponse::class);
-    Route::get('timecard/support', SupportTimecardController::class)->middleware(CacheResponse::class);
+    Route::get('timecard/integration', IntegrationTimecardController::class);
+    Route::get('timecard/support', SupportTimecardController::class);
 });
