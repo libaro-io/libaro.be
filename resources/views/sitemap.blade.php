3732d5e80f6f151d5f789919294d7f3806805dc0
Kevin Rousseau <kevinrousseau@Kevins-MacBook-Pro.local>
2025-10-06 09:39:25 +0200

    Add iot and robaws expertise pages to the sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,4 +1,4 @@
-@php use App\Http\Controllers\AboutUsController;use App\Http\Controllers\AssetsController;use App\Http\Controllers\BlogController;use App\Http\Controllers\ContactController;use App\Http\Controllers\CookiePolicyController;use App\Http\Controllers\DetailProductController;use App\Http\Controllers\DetailProjectController;use App\Http\Controllers\Expertises\AiIntegrationsExpertiseController;use App\Http\Controllers\Expertises\AppsExpertiseController;use App\Http\Controllers\Expertises\OdooExpertiseController;use App\Http\Controllers\Expertises\WebDevelopmentExpertiseController;use App\Http\Controllers\HomeController;use App\Http\Controllers\IntegrationTimecardController;use App\Http\Controllers\LandingPageController;use App\Http\Controllers\PrivacyPolicyController;use App\Http\Controllers\SupportTimecardController;use App\Http\Controllers\TermsController; @endphp
+@php use App\Http\Controllers\AboutUsController;use App\Http\Controllers\AssetsController;use App\Http\Controllers\BlogController;use App\Http\Controllers\ContactController;use App\Http\Controllers\CookiePolicyController;use App\Http\Controllers\DetailProductController;use App\Http\Controllers\DetailProjectController;use App\Http\Controllers\Expertises\AiIntegrationsExpertiseController;use App\Http\Controllers\Expertises\AppsExpertiseController;use App\Http\Controllers\Expertises\IOTExpertiseController;use App\Http\Controllers\Expertises\OdooExpertiseController;use App\Http\Controllers\Expertises\RobawsExpertiseController;use App\Http\Controllers\Expertises\WebDevelopmentExpertiseController;use App\Http\Controllers\HomeController;use App\Http\Controllers\IntegrationTimecardController;use App\Http\Controllers\LandingPageController;use App\Http\Controllers\PrivacyPolicyController;use App\Http\Controllers\SupportTimecardController;use App\Http\Controllers\TermsController; @endphp
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
     @foreach(config('app.supported_locales') as $locale)
         <url>
@@ -23,7 +23,13 @@
             <loc>{{ action(AiIntegrationsExpertiseController::class, ['locale' => $locale]) }}</loc>
         </url>
         <url>
+            <loc>{{ action(IOTExpertiseController::class, ['locale' => $locale]) }}</loc>
+        </url>
+        <url>
             <loc>{{ action(OdooExpertiseController::class, ['locale' => $locale]) }}</loc>
+        </url>
+        <url>
+            <loc>{{ action(RobawsExpertiseController::class, ['locale' => $locale]) }}</loc>
         </url>
         @foreach($blogs as $blog)
             <url>
