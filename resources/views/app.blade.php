9dcd6334f662711bc8d2bccc3bc57275a3ab83f9
Kevin Rousseau <kevinrousseau@Kevins-MacBook-Pro.local>
2025-10-09 10:41:44 +0200

    Add tag manager

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -56,6 +56,15 @@
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="fb:app_id" content="337068846453413">
 
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PZW6PW');</script>
+    <!-- End Google Tag Manager -->
+
     <link rel="preconnect" href="https://{{ config('filesystems.disks.s3.bucket') }}.s3.{{ config('filesystems.disks.s3.region') }}.amazonaws.com">
 
     <link rel="preload" as="image" crossorigin="anonymous" href="/images/header_striped.webp">
@@ -130,6 +139,11 @@
     @inertiaHead
 </head>
 <body class="font-sans antialiased">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PZW6PW"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 @inertia
 </body>
 </html>
