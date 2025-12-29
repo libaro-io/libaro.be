89db1749c11da7218c08b3e0db39a246c0af590e
JennisDev <jennis@libaro.be>
2025-12-29 14:54:45 +0100

    Update banner qr

--- routes/web.php
+++ routes/web.php
@@ -87,6 +87,8 @@ Route::prefix('{locale?}')->group(function () {
     Route::get('diensten/internet-of-things', fn () => Redirect::action(IOTExpertiseController::class, ['locale' => 'nl'], 301));
     Route::get('services/internet-of-things', fn () => Redirect::action(IOTExpertiseController::class, ['locale' => 'en'], 301));
 
+    Route::get('banner', fn () => Redirect::action(HomeController::class, ['locale' => 'nl']));
+
     Route::get('docs/{param1?}/{param2?}/{param3?}', fn () => abort(410));
     Route::get('cms', fn () => abort(410));
 });
