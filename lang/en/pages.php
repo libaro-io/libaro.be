a1eb94a8bb71cbe6d8464f868cdc300ca7632719
LaytonLibaro <layton@libaro.be>
2026-03-30 14:20:40 +0200

    Add project references to various pages

--- lang/en/pages.php
+++ lang/en/pages.php
@@ -479,6 +479,9 @@ return [
                 ],
             ],
         ],
+        'these_sectors' => [
+            'title' => 'Industries where we’ve implemented Robaws',
+        ],
         'api_integration' => [
             'title' => 'Libaro makes the connection with the Robaws API',
             'text' => 'Sometimes you can\'t just phase out certain programs, or Robaws seems to fall just short of making the complete switch. In that case, Libaro provides the perfect solution: we connect Robaws via the API with your existing or new software, so that your processes continue to run seamlessly and you still get maximum return from Robaws.',
@@ -517,6 +520,9 @@ return [
                     'description' => 'We streamline repetitive processes and create smart, AI-driven marketing content that saves time and increases impact.',
                 ],
             ],
+        ],
+        'these_sectors' => [
+            'title' => 'We have integrated AI for these sectors',
         ],
         'strategic_advantages' => [
             'title' => 'The strategic advantages of AI in your business',
